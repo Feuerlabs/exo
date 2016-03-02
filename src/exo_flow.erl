5d8f24bf4676ffa5602103a1c2687967349a75c8
Malotte Westman Lönne <malotte@malotte.net>
2016-03-02 20:41:22 +0100

    Corrected spelling mistake.

--- src/exo_flow.erl
+++ src/exo_flow.erl
@@ -117,9 +117,11 @@ stop() ->
 %%
 %% @end
 %%--------------------------------------------------------------------
--spec new(Key::term(), Policy::atom()) -> ok | {error, Error::atom()}.
+-spec new(Key::term() , Policy::atom()) -> 
+		 ok | 
+		 {error, Error::atom()}.
 
-new(Key, Policy) ->
+new(Key, Policy) when is_atom(Policy) ->
     case new_bucket({in, Key}, Policy) of
 	ok ->
 	    case new_bucket({out, Key}, Policy) of
@@ -178,7 +180,7 @@ fill({Direction, _K} = Key) when is_atom(Direction) ->
 	[B] when is_record(B, bucket) ->
 	   fill_bucket(B);
 	[] ->
-	   {error, unkown_key}
+	   {error, unknown_key}
     end.
 
 %%--------------------------------------------------------------------
@@ -198,7 +200,7 @@ fill_time({Direction, _K} = Key, Tokens)
 	[B] when is_record(B, bucket) ->
 	   bucket_fill_time(B, Tokens);
 	[] ->
-	    {error, unkown_key}
+	    {error, unknown_key}
     end.
 
 
@@ -220,7 +222,7 @@ wait({Direction, _K} = Key, Tokens)
 	[{Key, B}] when is_record(B, bucket) ->
 	   bucket_wait(B, Tokens);
 	[] ->
-	    {error, unkown_key}
+	    {error, unknown_key}
     end.
 	    	    
 %%--------------------------------------------------------------------
@@ -243,7 +245,7 @@ fill_wait({Direction, _K} = Key, Tokens)
 	   bucket_wait(B, Tokens),
 	   fill_bucket(B);
 	[] ->
-	    {error, unkown_key}
+	    {error, unknown_key}
     end.
 
 %%--------------------------------------------------------------------
@@ -434,7 +436,7 @@ use_tokens({out, _K} = Key, Tokens) ->
 	    lager:debug("bucket ~p full, ~p.", [Key, Action]),
 	    {action, Action};
 	[] ->
-	    {error, unkown_key}
+	    {error, unknown_key}
     end.
 
 fill_bucket(B) when is_record(B, bucket) ->
